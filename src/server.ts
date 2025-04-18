6a6636832145ac5e5d0a2590752a4e22267bafda
NikitaLiman <fram08fort29@gmail.com>
2025-04-18 13:52:38 +0300

    changed domen

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://swift-chat-fawn.vercel.app/",
+    origin: "https://swift-chat-fawn.vercel.app",
     methods: ["GET", "POST"],
   },
 });
